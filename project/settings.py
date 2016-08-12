6a5a1f5d1d76fb361e27f2b8cb7cf330e8fd5a6a
harryomharry <harjot4u@gmail.com>
2016-08-12 15:03:02 -0400

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/' # You may find this is already defined as such.
